a16eb2534571683736741ff144e522cdf86c4b66
Barrel <visvam64@gmail.com>
2023-04-27 17:30:38 -0400

    Added legs to transactions.

--- core/transaction.js
+++ core/transaction.js
@@ -1,16 +1,32 @@
+/**
+ * A Transaction represents a specific transaction to a location on campus.
+ */
+
 const DataIntegrity =  require('./data_integrity');
 
 class Transaction {
 
+    /**
+     * LEGS ARRAY
+     * STUDENT ID
+    */
     constructor(aLocation, aStudentID) {
         this.location = aLocation;
         this.studentID = parseInt(aStudentID);
+        this.legs = [new Leg("IN", aLocation, new Date(Date.now)), new Leg("OUT", aLocation, new Date(Date.now))]; 
+        this.isComplete = () => { 
+            var result = false;
+            this.legs.forEach(leg => result = result && leg.isComplete);
+            return result;
+         }
         if ( this.studentID == null || this.studentID == 0 ) {
             DataIntegrity.addIssue("ERROR","Location","constructor","Location Contructor adding empty StudentID for->" + JSON.stringify(this));
         }
         if ( this.location == null ) {
             DataIntegrity.addIssue("ERROR", "Location","constructor","Location Contructor adding empty location for->" + JSON.stringify(this));
         }		
-    }	
+    }
+    
+    
     
 }
