23ab1f66accd796e51534ea199b8c978c6abf981
HCN <h1c3nn@gmail.com>
2023-04-27 23:08:00 -0400

    removed useless things

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -8,8 +8,7 @@ const setRoom = document.getElementById("setRoom");
 const clearStaff = document.getElementById("clearStaff");
 const checkoutwindow = document.getElementById("checkoutwindow");
 const checkoutbutton = document.getElementById("checkout");
-const time = "http://worldtimeapi.org/api/timezone/America/New_York";
-//store list of current users in local storage or empty list
+
 var currentUsers = JSON.parse(localStorage.getItem("currentUsers")) || [];
 var names = JSON.parse(localStorage.getItem("names")) || [];
 var staffList = JSON.parse(localStorage.getItem("staffList")) || [];
@@ -22,16 +21,12 @@ function hideElement(id) {
 function showElement(id, display) {
     document.getElementById(id).style.display = display;
 }
-//create list element and add it to userlis
-//display all checked out users on screen
+
 function loadUsers() {
-    //clear everything
     if(!userlist) return;
     userlist.innerHTML = '';
-    //load the user count
     userCount.textContent = "Students out: " + currentUsers.length;
 
-    //load all user from currentUsers 
     for(var i = 0; i < currentUsers.length; i++) {
         var user = currentUsers[i];
         var name = names[i];
@@ -39,27 +34,45 @@ function loadUsers() {
         var resolveButton = document.createElement("button");
         var userID = document.createElement("label");
         var userName = document.createElement("label");
+        var timeIn = document.createElement("label");
+        //var elapsedTime = document.createElement("label");
+        var status = document.createElement("label");
         resolveButton.textContent = "Resolve";
+        timeIn.textContent = getTime();
         userID.textContent = user;
         userName.textContent = name;
+        status.textContent = "pending";
+        status.style.color = "green";
         resolveButton.classList.add("resButton");
         li.classList.add("inner");
         userID.classList.add("userinfo");
         userName.classList.add("userinfo");
+        timeIn.classList.add("userinfo");
+        status.classList.add("userinfo");
+        //elapsedTime.classList.add("userinfo");
 
-
-        //onclick for resolve button
         resolveButton.onclick = function() {
             removeUser(user);
-            console.log("bruh this better work");
         }
 
         li.appendChild(userName);
         li.appendChild(userID);
+        li.appendChild(timeIn);
+        //li.appendChild(elapsedTime);
+        li.appendChild(status);
         li.appendChild(resolveButton);
         userlist.appendChild(li);
     }
 
+}
+
+function getTime() {
+    const time1 = new Date();
+    const hr = time1.getHours();
+    const min = time1.getMinutes();
+    const sec = time1.getSeconds();
+    
+    return `${hr}:${min}:${sec}`;
 }
 
 
@@ -91,13 +104,11 @@ function addStaff(n) {
 }
 
 clearStaff.addEventListener("click", function() {
-    console.log("here");
     staffList.length = 0;
     localStorage.setItem("staffList", JSON.stringify(staffList));
     console.log(staffList);
 });
 
-//show pop up window and current time
 startButton.addEventListener("click",  function() {
     id = document.getElementById("idNum").value;
     name = document.getElementById("flname").value;
@@ -105,15 +116,9 @@ startButton.addEventListener("click",  function() {
         alert("Enter your ID please");
         return;
     }
-    console.log(id);
     document.getElementById("idNum").value = '';
     document.getElementById("flname").value = '';
     checkoutwindow.style.display = "flex";
-
-    //wait for getTime to fetch data
-    getTime().then(time => {
-        document.getElementById("time").textContent = time;
-    });
 });
 
 
@@ -131,27 +136,12 @@ setRoom.addEventListener("click", function() {
     document.getElementById('staffName').value = null; 
     document.getElementById("displayRoomNum").textContent = "Room: " + document.getElementById("roomNum").value;
     addStaff(document.getElementById("staffName").value);
-})
+});
 
-//adds user
-/*
-id number, first and last name, time in, start a timer, update the timer
-*/
 checkout.addEventListener("click", function() {
     checkoutwindow.style.display = "none";
     addUser(id, name);
-    console.log("bruh");
 });
-
-async function getTime() {
-    const response = await fetch(time);
-    const timeData = await response.json();
-    const currTime = new Date(timeData.datetime);
-    var hr = currTime.getHours();
-    var min = currTime.getMinutes();
-
-    return hr + ":" + min;
-}
 
 
 
